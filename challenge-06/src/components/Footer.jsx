c13250e5cad80a0965f5114a416af30f2140e092
Vil <101118033+villeneuveandhira@users.noreply.github.com>
2023-11-06 21:17:14 +0700

    Update Footer.jsx

--- challenge-06/src/components/Footer.jsx
+++ challenge-06/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      &copy; 2023 Binar Academy - (challenge-05) Villeneuve Andhira Suwandhi FEJS-1
+      &copy; 2023 Binar Academy - (challenge-06) Villeneuve Andhira Suwandhi FEJS-1
     </footer>
   );
 }
